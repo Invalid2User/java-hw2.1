5545f585592e56a26c247aa1422979af63540654
Invalid2User <lapina.gen@gmail.com>
2022-05-11 14:05:03 +0300

    Commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost = 537;
+        int requirement = 20;
+        int bonusMiles = cost / requirement;
+
+        System.out.println("Вам начислено " + bonusMiles + " бонусных миль! ");
+    }
+}
